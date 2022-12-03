ca926970d969c5eeea3ada261b88df8f72471d9e
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-03 09:54:11 +0100

    day 3 cleanup

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,44 +1,20 @@
-class Day3(input: List<String>) {
+class Day3(private val input: List<String>) {
 
-    val r1 = input.map { it.substring(0, it.length / 2).toCharArray().toSet() to it.substring(it.length / 2).toCharArray().toSet()   }
-    val r2 = r1.map { (p1, p2) -> p1.intersect(p2) }
-    val r3 = r2.map { it.toTypedArray()[0] }
-    val r4 = r3.map { 
-        if(it.isLowerCase()) {
-             it.code - 96
-        } else {
-            it.code - 38 
-        }
+    fun part1() = solve(input.map { elements -> elements.chunked(elements.length / 2) })
+
+    fun part2() = solve(input.chunked(3))
+
+    private fun solve(groups: List<List<String>>) = groups
+        .map { group -> commonItem(group) }
+        .sumOf { commonItem -> score(commonItem) }
+
+    private fun commonItem(group: List<String>) = group
+        .map { items -> items.toSet() }
+        .reduce { prev, curr -> prev.intersect(curr) }
+        .first()
+
+    private fun score(item: Char) = when (item.isLowerCase()) {
+        true -> item.code - 96
+        false -> item.code - 38
     }
-    
-    val p1 = input.map { it.toCharArray().toList() }
-    val p2 = p1.chunked(3);
-    val p3 = p2.map { (t1, t2, t3) -> t1.intersect(t2).intersect(t3) }
-    val p4 = p3.map { it.toTypedArray()[0] }
-    val p5 = p4.map {
-        if(it.isLowerCase()) {
-            it.code - 96
-        } else {
-            it.code - 38
-        }
-    }
-    
-    fun part1() = r4.sum()
-
-    fun part2() = p5.sum()
-}
-
-fun main() {
-
-    val input = readLines("day3.txt")
-//    val input = readLines("day3.txt", true)
-    
-    val result = Day3(input).part2()
-    
-    
-    println('a'.code - 96)
-    println('A'.code - 38)
-
-
-    println(result)
 }
