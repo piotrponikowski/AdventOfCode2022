a5aa58324ed82660a4e25586256aeb90528af35e
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-04 06:18:52 +0100

    day 4 cleanup

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -6,9 +6,7 @@ class Day4(input: List<String>) {
         .map { (from1, to1, from2, to2) -> from1.toInt()..to1.toInt() to from2.toInt()..to2.toInt() }
         .map { (range1, range2) -> range1.toSet() to range2.toSet() }
 
-
     fun part1() = pairs.count { (range1, range2) -> range1.containsAll(range2) || range2.containsAll(range1) }
 
     fun part2() = pairs.count { (range1, range2) -> range1.intersect(range2).isNotEmpty() }
-
 }
