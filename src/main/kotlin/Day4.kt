0b03250e04594206127a966af99c282d683b00b5
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-04 07:40:06 +0100

    day 4 cleanup

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,9 +1,8 @@
 class Day4(input: List<String>) {
 
-    private val pattern = Regex("""^(\d+)-(\d+),(\d+)-(\d+)$""")
-
-    private val pairs = input.map { pattern.matchEntire(it)!!.destructured }
-        .map { (from1, to1, from2, to2) -> from1.toInt()..to1.toInt() to from2.toInt()..to2.toInt() }
+    private val pairs = input
+        .map { line -> line.split(",").map { section -> section.toInt() } }
+        .map { (from1, to1, from2, to2) -> from1..to1 to from2..to2 }
         .map { (range1, range2) -> range1.toSet() to range2.toSet() }
 
     fun part1() = pairs.count { (range1, range2) -> range1.containsAll(range2) || range2.containsAll(range1) }
