816ffebf8f9772d995c7d385fccfbc82130ad2c8
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-06 06:14:16 +0100

    day 6 cleanup

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,11 +1,11 @@
 class Day6(val input: String) {
-    
+
     private fun solve(size: Int) = input
         .windowed(size)
-        .mapIndexed { index, letters -> index to (letters.toSet().size == size) }
-        .first { (_, match) -> match }
+        .mapIndexed { index, letters -> index to letters }
+        .first { (_, letters) -> letters.toSet().size == size }
         .let { (index, _) -> index + size }
-    
+
     fun part1() = solve(4)
 
     fun part2() = solve(14)
