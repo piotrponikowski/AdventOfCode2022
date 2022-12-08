53b62f35aa54b12d95abe5c145bb231a77f9a477
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-08 18:06:25 +0100

    day 8 cleanup

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -1,84 +1,43 @@
 class Day8(input: List<String>) {
 
-    val trees = input.map { line -> line.map { value -> value.toString().toInt() } }
+    private val trees = input.map { line -> line.map { value -> value.toString().toInt() } }
         .flatMapIndexed { y, line -> line.mapIndexed { x, risk -> Point(x, y) to risk } }.toMap()
 
-    fun solve() = trees.map { tree ->
+    fun part1() = trees
+        .filter { (treeLocation, treeSize) ->
+            findTreeLines(treeLocation)
+                .any { line -> line.all { otherLocation -> trees[otherLocation]!! < treeSize } }
+        }.count()
 
-        var isAnyVisible = false
+    fun part2() = trees
+        .map { (treeLocation, treeSize) ->
+            findTreeLines(treeLocation)
+                .map { line -> countVisible(line, treeSize) }
+                .reduce { score, distance -> score * distance }
+        }.max()
 
-        directions.forEach { dir ->
+    private fun findTreeLines(start: Point) = directions.map { direction -> pointsInDirection(start, direction) }
 
-            var isVisible = true
+    private fun pointsInDirection(current: Point, direction: Point, points: List<Point> = listOf()): List<Point> {
+        val next = current + direction
 
-            var otherPoint = tree.key
-            while (otherPoint != null) {
-                otherPoint += dir
-
-                val otherValue = trees[otherPoint] ?: break
-
-                if (otherValue >= tree.value) {
-                    isVisible = false
-                }
-            }
-
-            isAnyVisible = isAnyVisible || isVisible
+        return when (trees.containsKey(next)) {
+            true -> pointsInDirection(next, direction, points + next)
+            else -> points
         }
-
-        tree.key to isAnyVisible
     }
 
-    fun solve2() = trees.map { tree ->
-
-        var totalScore = 1
-        
-        if(tree.key.x == 2 && tree.key.y ==3) {
-            println()
+    private fun countVisible(line: List<Point>, size: Int): Int {
+        return when {
+            line.isEmpty() -> 0
+            trees[line.first()]!! < size -> countVisible(line.drop(1), size) + 1
+            else -> 1
         }
-
-        directions.forEach { dir ->
-
-            var score = 0
-
-            var otherPoint = tree.key
-            while (otherPoint != null) {
-                otherPoint += dir
-
-                val otherValue = trees[otherPoint] ?: break
-
-                if (otherValue < tree.value) {
-                    score += 1
-                } else {
-                    score += 1
-                    break
-                }
-            }
-
-            totalScore *= score
-        }
-
-        tree.key to totalScore
     }
-
-
-    fun part1() = solve().count { it.second }
-
-    fun part2() = solve2().map { it.second }.max()
 
     private val directions = listOf(Point(-1, 0), Point(1, 0), Point(0, -1), Point(0, 1))
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Point) = Point(x + other.x, y + other.y)
     }
-}
-
-fun main() {
-
-    val input = readLines("day8.txt")
-//    val input = readLines("day8.txt", true)
-
-    val result = Day8(input).solve2().map { it.second }
-        .max()
-    println(result)
-
 }
