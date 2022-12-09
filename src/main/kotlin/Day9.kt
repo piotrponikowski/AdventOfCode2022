4f0d1ef59d62aa7f756030a5bf1b60c27a4915bc
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-09 21:10:59 +0100

    day 9 cleanup

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -5,48 +5,46 @@ class Day9(input: List<String>) {
 
     private val instructions = input.map { it.split(" ") }
         .map { (a, b) -> Direction.valueOf(a) to b.toInt() }
-
-    var visited = mutableSetOf<Point>()
-
-    fun solve(size: Int): Int {
-//        var head = Point(0, 0)
-        var rope = (0 until size).map { Point(0, 0) }.toMutableList()
-
-        instructions.forEach { (dir, steps) ->
-
-            (1..steps).forEach { step ->
-                val head = rope.first() + dir
-
-                rope[0] = head
-
-                (0 until size - 1).forEach { index ->
-                    val h = rope[index]
-                    val t = rope[index + 1]
-
-                    rope[index + 1] = moveTail(h, t)
-
-                }
-
-                visited += rope.last()
-
-            }
-        }
-
-        return visited.size
-
-    }
-
-    private fun moveTail(head: Point, tail: Point) =
-        when (head.isNextTo(tail)) {
-            true -> tail
-            false -> Point(tail.x + (head.x - tail.x).sign, tail.y + (head.y - tail.y).sign)
-        }
-
-
+    
     fun part1() = solve(2)
 
     fun part2() = solve(10)
 
+    fun solve(size: Int) = instructions
+        .fold(initRope(size) to initVisited()) { (rope, visited), (direction, steps) ->
+            executeInstruction(direction, steps, rope, visited)
+        }.let { (_, visited) -> visited.count() }
+
+    private fun initRope(size: Int) = (0 until size).map { Point(0, 0) }
+
+    private fun initVisited() = setOf<Point>()
+
+    private fun executeInstruction(direction: Direction, steps: Int, rope: List<Point>, visited: Set<Point>) =
+        (0 until steps).fold(rope to visited) { (ropeStep, visitedStep), _ ->
+            moveTail(ropeStep, direction)
+                .let { newRope -> newRope to visitedStep + newRope.last() }
+        }
+
+    private fun moveTail(rope: List<Point>, direction: Direction, newRope: List<Point> = listOf()): List<Point> {
+        return if (newRope.isEmpty()) {
+            moveTail(rope.drop(1), direction, listOf(rope.first() + direction))
+
+        } else if (rope.isNotEmpty()) {
+            val currentHead = newRope.last()
+            val currentTail = rope.first()
+            val adjustedTail = adjustTail(currentHead, currentTail)
+
+            moveTail(rope.drop(1), direction, newRope + adjustedTail)
+        } else {
+            newRope
+        }
+    }
+
+    private fun adjustTail(head: Point, tail: Point) =
+        when (head.isNextTo(tail)) {
+            true -> tail
+            false -> Point(tail.x + (head.x - tail.x).sign, tail.y + (head.y - tail.y).sign)
+        }
 
     enum class Direction(val x: Int, val y: Int) {
         L(-1, 0), R(1, 0), U(0, -1), D(0, 1)
@@ -57,14 +55,4 @@ class Day9(input: List<String>) {
 
         fun isNextTo(other: Point) = abs(x - other.x) <= 1 && abs(y - other.y) <= 1
     }
-}
-
-fun main() {
-
-    val input = readLines("day9.txt")
-//    val input = readLines("day9.txt", true)
-
-    val result = Day9(input).part1()
-    println(result)
-
 }
