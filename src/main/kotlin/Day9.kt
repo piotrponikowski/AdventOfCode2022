09b4a213c45619c74e4300c79397471ccd917118
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-09 08:46:21 +0100

    day 9 cleanup

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,120 +1,51 @@
+import kotlin.math.abs
+import kotlin.math.sign
+
 class Day9(input: List<String>) {
 
-    val instructions = input.map { it.split(" ") }
+    private val instructions = input.map { it.split(" ") }
         .map { (a, b) -> Direction.valueOf(a) to b.toInt() }
-
 
     var visited = mutableSetOf<Point>()
 
-    fun solve(size:Int) :Int {
+    fun solve(size: Int): Int {
 //        var head = Point(0, 0)
         var rope = (0 until size).map { Point(0, 0) }.toMutableList()
 
         instructions.forEach { (dir, steps) ->
-            
+
             (1..steps).forEach { step ->
                 val head = rope.first() + dir
 
                 rope[0] = head
 
-                (0 until size-1).forEach { index -> 
+                (0 until size - 1).forEach { index ->
                     val h = rope[index]
-                    val t = rope[index+1]
-                    
-                    rope[index+1] = adjust(h,t)
+                    val t = rope[index + 1]
+
+                    rope[index + 1] = moveTail(h, t)
 
                 }
 
                 visited += rope.last()
-   
+
             }
         }
-        
+
         return visited.size
 
     }
 
-    fun adjust(head: Point, tail: Point): Point {
-        var b = tail
-
-        if (head.x - tail.x == 2 && head.y == tail.y) {
-            b += Direction.R
-        } else if (head.x - tail.x == -2 && head.y == tail.y) {
-            b += Direction.L
-        } else if (head.y - tail.y == 2 && head.x == tail.x) {
-            b += Direction.D
-        } else if (head.y - tail.y == -2 && head.x == tail.x) {
-            b += Direction.U
+    private fun moveTail(head: Point, tail: Point) =
+        when (head.isNextTo(tail)) {
+            true -> tail
+            false -> Point(tail.x + (head.x - tail.x).sign, tail.y + (head.y - tail.y).sign)
         }
 
-        if (tail + Direction.R + Direction.R + Direction.U == head) {
-            b += Direction.R
-            b += Direction.U
-        } else if (tail + Direction.R + Direction.R + Direction.D == head) {
-            b += Direction.R
-            b += Direction.D
-        }
-
-        if (tail + Direction.L + Direction.L + Direction.U == head) {
-            b += Direction.L
-            b += Direction.U
-        } else if (tail + Direction.L + Direction.L + Direction.D == head) {
-            b += Direction.L
-            b += Direction.D
-        }
-
-        if (tail + Direction.U + Direction.U + Direction.L == head) {
-            b += Direction.U
-            b += Direction.L
-        } else if (tail + Direction.U + Direction.U + Direction.R == head) {
-            b += Direction.U
-            b += Direction.R
-        }
-
-        if (tail + Direction.D + Direction.D + Direction.L == head) {
-            b += Direction.D
-            b += Direction.L
-        } else if (tail + Direction.D + Direction.D + Direction.R == head) {
-            b += Direction.D
-            b += Direction.R
-        }
-
-        if (tail + Direction.R + Direction.R + Direction.U + Direction.U  == head) {
-            b += Direction.R
-            b += Direction.U
-        } else if (tail + Direction.R + Direction.R + Direction.D + Direction.D  == head) {
-            b += Direction.R
-            b += Direction.D
-        }
-
-        if (tail + Direction.L + Direction.L + Direction.U + Direction.U  == head) {
-            b += Direction.L
-            b += Direction.U
-        } else if (tail + Direction.L + Direction.L + Direction.D + Direction.D  == head) {
-            b += Direction.L
-            b += Direction.D
-        }
-        
-        return b
-    }
 
     fun part1() = solve(2)
 
     fun part2() = solve(10)
-
-
-    private fun printPoints(state: Set<Point>): String {
-        val xMax = state.maxOf { point -> point.x }
-        val xMin = state.minOf { point -> point.x }
-        val yMax = state.maxOf { point -> point.y }
-        val yMin = state.minOf { point -> point.y }
-
-        return (yMin..yMax).joinToString("\n") { y ->
-            (xMin..xMax).joinToString("") { x ->
-                if (Point(x, y) in state) "#" else " "
-            }
-        }
-    }
 
 
     enum class Direction(val x: Int, val y: Int) {
@@ -123,6 +54,8 @@ class Day9(input: List<String>) {
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Direction) = Point(x + other.x, y + other.y)
+
+        fun isNextTo(other: Point) = abs(x - other.x) <= 1 && abs(y - other.y) <= 1
     }
 }
 
