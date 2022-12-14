6f60b5b385905239c9583489337a47dc419ea812
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-14 08:03:46 +0100

    day 14 tests

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -2,110 +2,89 @@ import kotlin.math.sign
 
 class Day14(input: List<String>) {
 
-    val groups = input.map { line ->
-        line.split(" -> ")
-            .map { operation -> operation.split(",") }
-            .map { (x, y) -> Point(x.toInt(), y.toInt()) }
-    }
+    private val groups = input.map { line -> parseLine(line) }
 
-    val board = mutableSetOf<Point>()
-    val sands = mutableSetOf<Point>()
+    private val board = groups
+        .flatMap { group -> group.windowed(2).flatMap { (a, b) -> linePoints(a, b) } }
+        .associateWith { '#' }
+        .toMutableMap()
 
-    init {
-        prepareBoard()
-    }
+    private val maxY = board.keys.maxOf { point -> point.y + 2 }
 
-    fun prepareBoard() {
-        groups.forEach { group ->
-            group.windowed(2).forEach { (a, b) ->
-                val dx = (b.x - a.x).sign
-                val dy = (b.y - a.y).sign
-                val d = Point(dx, dy)
+    private fun parseLine(line: String) = line.split(" -> ")
+        .map { operation -> operation.split(",") }
+        .map { (x, y) -> Point(x.toInt(), y.toInt()) }
 
-                var current = a
-                board += current
-
-                while (current != b) {
-                    current += d
-                    board += current
-                }
-            }
+    private fun linePoints(from: Point, to: Point, result: List<Point> = listOf()): List<Point> {
+        return if (from == to) {
+            result + from
+        } else {
+            val direction = Point((to.x - from.x).sign, (to.y - from.y).sign)
+            val nextFrom = from + direction
+            linePoints(nextFrom, to, result + from)
         }
-
-        println(printPoints())
     }
 
-    val down = Point(0, 1)
-    val left = Point(-1, 1)
-    val right = Point(1, 1)
+    private fun drop(current: Point = Point(500, 0)): Point {
+        val next = directions
+            .map { direction -> current + direction }
+            .firstOrNull { point -> point !in board }
 
+        return if (next != null && next.y < maxY) {
+            drop(next)
+        } else {
+            current
+        }
+    }
 
-    fun dropSand(): Point {
-        var sand = start
-        val fullState = board + sands
-
+    fun part1(): Int {
         while (true) {
-            var next = sand
-            if (sand + down !in fullState) {
-                next += down
-            } else if (sand + left !in fullState) {
-                next += left
-            } else if (sand + right !in fullState) {
-                next += right
-            }
-
-            if (next == sand) {
-                sands += sand
+            val point = drop()
+            
+            if (point.y + 1 == maxY) {
                 break
-            } else if(next.y == maxY){
-                sands += sand
-                break
-            }else {
-                sand = next
+            } else {
+                board[point] = 'o'
             }
-        }
-
-        return sand
-    }
-
-    val start = Point(500, 0)
-    val maxY = board.maxOf { it.y } + 2
-
-    fun part1() {
-
-        while (true) {
-            sands += dropSand()
+            
             //println(printPoints())
-            println(sands.size)
+            //println(board.count { (key, value) -> value == 'o' })
         }
+        return board.count { (key, value) -> value == 'o' }
     }
 
-    fun part2() {
-
+    fun part2(): Int {
         while (true) {
-            sands += dropSand()
+            val point = drop()
+
+            if (point == Point(500, 0)) {
+                board[point] = 'o'
+                break
+            } else {
+                board[point] = 'o'
+            }
+
             //println(printPoints())
-            println(sands.size)
+            //println(board.count { (key, value) -> value == 'o' })
         }
+        return board.count { (key, value) -> value == 'o' }
     }
-
-
-    private fun printPoints(): String {
-        val xMax = board.maxOf { point -> point.x }
-        val xMin = board.minOf { point -> point.x }
-        val yMax = board.maxOf { point -> point.y }
-        val yMin = board.minOf { point -> point.y }
+    
+    fun printPoints(): String {
+        val xMax = board.keys.maxOf { point -> point.x }
+        val xMin = board.keys.minOf { point -> point.x }
+        val yMax = board.keys.maxOf { point -> point.y }
+        val yMin = board.keys.minOf { point -> point.y }
 
         return (yMin..yMax).joinToString("\n") { y ->
             (xMin..xMax).joinToString("") { x ->
-                when (Point(x, y)) {
-                    in board -> "#"
-                    in sands -> "o"
-                    else -> " "
-                }
+                (board[Point(x, y)] ?: ' ').toString()
             }
         }
     }
+
+
+    private val directions = listOf(Point(0, 1), Point(-1, 1), Point(1, 1))
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Point) = Point(x + other.x, y + other.y)
@@ -114,11 +93,11 @@ class Day14(input: List<String>) {
 
 fun main() {
 
-    val input = readLines("day14.txt")
-//    val input = readLines("day14.txt", true)
+//    val input = readLines("day14.txt")
+    val input = readLines("day14.txt", true)
 
     val day = Day14(input)
-    val result = day.part2()
+    val result = day.part1()
 
     println(result)
 
