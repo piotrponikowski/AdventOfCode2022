5d35a2ac0e93590aa526cb1550f350e0b98e2150
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-17 07:44:57 +0100

    day 17 part 2

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -23,43 +23,102 @@ class Day17(input: String) {
 
         val board = mutableSetOf<Point>()
         var directionCounter = 0
-        repeat(2022) { blockCounter ->
+
+        val seen = mutableListOf<Pair<Condition, Int>>()
+        var score = 0
+        var blockCounter = 0
+        while (true) {
+
             val top = board.maxOfOrNull { point -> point.y } ?: -1
             var block = blocks[blockCounter % blocks.size].map { point -> Point(point.x + 2, point.y + (top + 4)) }
 
-            while(true) {
-                val direction = directions[directionCounter % directions.size ]
+
+            val blockSignature = blockCounter % blocks.size
+            val directionSignature = directionCounter % directions.size
+            val boardSignature = signature(board)
+
+            val id = Condition(blockSignature, directionSignature, boardSignature)
+            if (seen.find { it.first == id } != null) {
+                println("loop")
+                part2Score(id, seen)
+                
+                break
+            }
+
+            seen += id to score
+
+            while (true) {
+                val direction = directions[directionCounter % directions.size]
                 directionCounter += 1
                 //println(direction)
 
                 //println(printPoints(board + block))
-                
-                val movedBlock = block.map { it + direction } 
+
+                val movedBlock = block.map { it + direction }
                 val movedMaxX = movedBlock.maxOf { it.x }
                 val movedMinX = movedBlock.minOf { it.x }
 
-                if(movedBlock.intersect(board).isEmpty() && movedMinX >=0 && movedMaxX <= 6) {
+                if (movedBlock.intersect(board).isEmpty() && movedMinX >= 0 && movedMaxX <= 6) {
                     block = movedBlock
                 }
-                
+
                 val droppedBlock = block.map { it + down }
-                if(droppedBlock.intersect(board).isEmpty() && droppedBlock.minOf { it.y } >= 0) {
+                if (droppedBlock.intersect(board).isEmpty() && droppedBlock.minOf { it.y } >= 0) {
                     block = droppedBlock
-                    
+
                 } else {
                     board += block
                     break
                 }
             }
-            
+
 //            println()
 //            println(printPoints(board))
 //            println()
-        }
-        
-        val score = board.maxOf { it.y }
-        println(score + 1)
+            score = board.maxOf { it.y } + 1
 
+            blockCounter++
+        }
+
+
+//        val count = 1000000000000L
+//        val loopScore = seen[seen.size - 2].second
+//        val loopSize = seen.size - 1
+//
+//        val willRepeat = count / loopSize
+//
+//        val result = loopScore * willRepeat
+//        println(result)
+//        println()
+
+    }
+
+    private fun part2Score(id: Condition, seen: List<Pair<Condition, Int>>) {
+        val loopStartIndex = seen.indexOfFirst { it.first == id }
+        val loopSize = seen.size - loopStartIndex
+        
+        val loopStartScore = seen[loopStartIndex-1].second
+        val loopScore = seen.last().second - loopStartScore
+
+        val blocks = 1000000000000L
+        
+        val loopsCount = (blocks - loopStartIndex) / loopSize
+
+
+        val remainingCount = (blocks - loopStartIndex) % loopSize
+        val remainingScore = seen[loopStartIndex + remainingCount.toInt()].second - loopStartScore
+        
+        val score = loopStartScore + (loopsCount * loopScore) + remainingScore
+        
+        println(score)
+        println()
+    }
+
+    private fun signature(board: Set<Point>): List<Int> {
+        val sign = (0..6).map { x -> board.filter { it.x == x }.maxOfOrNull { it.y } ?: 0 }
+        val min = sign.min()
+
+        return sign.map { it - min }
     }
 
     fun part1() {
@@ -82,7 +141,9 @@ class Day17(input: String) {
             }
         }
     }
-    
+
+    data class Condition(val block: Int, val direction: Int, val top: List<Int>)
+
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Point) = Point(x + other.x, y + other.y)
     }
@@ -93,7 +154,7 @@ fun main() {
     val input = readText("day17.txt")
 //    val input = readText("day17.txt", true)
 
-    val result = Day17(input).part1()
+    val result = Day17(input).part2()
     println(result)
 
 }
