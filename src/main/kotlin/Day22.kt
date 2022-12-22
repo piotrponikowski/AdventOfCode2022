bb4db0a98513d4372e453a93e28e0314aae14934
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-22 08:59:18 +0100

    day 22 part 2 (before cleanup)

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -26,111 +26,140 @@ class Day22(input: String) {
     val startingPoint = Point(startingX, startingY)
     val startingDirection = Point(1, 0)
 
-    fun solve():Int {
+    fun solve(): Int {
         var position = startingPoint
         var direction = startingDirection
 
         println("Position: $position, direction: $direction")
-        
+
         for (instruction in instructions) {
             println("Instruction: $instruction")
-            
+
             if (instruction == "R") {
                 direction = Point(-direction.y, direction.x)
             } else if (instruction == "L") {
                 direction = Point(direction.y, -direction.x)
             } else {
-                
+
                 val steps = instruction.toInt()
                 repeat(steps) {
                     val newPosition = position + direction
                     val newSymbol = board[newPosition]
-                    if(newSymbol == null) {
-                        
-                        var wrapX = newPosition.x
-                        var wrapY = newPosition.y
+                    if (newSymbol == null) {
 
-                        if(direction == right) {
-                            wrapX = board.filter { it.key.y == newPosition.y }.minOf { it.key.x }
-                        } else if(direction == left) {
-                            wrapX = board.filter { it.key.y == newPosition.y }.maxOf { it.key.x }
-                        } else  if(direction == down) {
-                            wrapY = board.filter { it.key.x == newPosition.x }.minOf { it.key.y }
-                        } else if(direction == up) {
-                            wrapY = board.filter { it.key.x == newPosition.x }.maxOf { it.key.y }
+                        val (wrapPosition, wrapDirection) = wrap(position, direction)
+
+                        if (board[wrapPosition]!! == '.') {
+                            position = wrapPosition
+                            direction = wrapDirection
                         }
-                        
-                        val wrapPosition = Point(wrapX, wrapY)
-                        if(board[wrapPosition]!! == '.') {
-                            position = wrapPosition  
-                        }
-                    } else if(newSymbol == '.') {
+                    } else if (newSymbol == '.') {
                         position = newPosition
-                    } 
+                    }
                 }
-                
+
             }
-            
+
             println("Position: $position, direction: $direction")
             println()
         }
-        
+
         var directionScore = 0
-        if(direction == right) {
+        if (direction == right) {
             directionScore = 0
-        } else if(direction == down) {
+        } else if (direction == down) {
             directionScore = 1
-        } else if(direction == left) {
+        } else if (direction == left) {
             directionScore = 2
-        } else if(direction == up) {
+        } else if (direction == up) {
             directionScore = 3
         }
-        
-        val score = ((position.y+1) * 1000) + ((position.x+1) * 4) + directionScore
-        
+
+        val score = ((position.y + 1) * 1000) + ((position.x + 1) * 4) + directionScore
+
         println()
-        
-        
+
+
         return score
     }
 
-    fun part1() =solve()
-    
+    fun part2() = solve()
 
-    fun part2()  {
-        val xMax = board.keys.maxOf { point -> point.x }
-        val xMin = board.keys.minOf { point -> point.x }
-        val yMax = board.keys.maxOf { point -> point.y }
-        val yMin = board.keys.minOf { point -> point.y }
+    fun wrap(position: Point, direction: Point): Pair<Point, Point> {
+        println("Moving outside from: $position, direction: $direction")
 
-        val width = xMax+1
-        val height = yMax+1
-        
-        val cubeSize= width / 4
+        val positionX = position.x
+        val positionY = position.y
 
+        var wrapPosition: Point? = null
+        var wrapDirection: Point? = null
 
-        println()
-    }
-
-
-    private fun printPoints(state: Map<Point, Char>): String {
-        val xMax = state.keys.maxOf { point -> point.x }
-        val xMin = state.keys.minOf { point -> point.x }
-        val yMax = state.keys.maxOf { point -> point.y }
-        val yMin = state.keys.minOf { point -> point.y }
-
-        return (yMin..yMax).joinToString("\n") { y ->
-            (xMin..xMax).joinToString("") { x ->
-                (state[Point(x, y)] ?: ' ').toString()
+        if (direction == up) {
+            if (positionX in 0..49 && positionY == 100) {
+                wrapPosition = Point(50, 50 + positionX)
+                wrapDirection = right
+            } else if (positionX in 50..99 && positionY == 0) {
+                wrapPosition = Point(0, 150 + (positionX - 50))
+                wrapDirection = right
+            } else if (positionX in 100..149 && positionY == 0) {
+                wrapPosition = Point(0 + (positionX - 100), 199)
+                wrapDirection = up
             }
         }
+
+        if (direction == down) {
+            if (positionX in 0..49 && positionY == 199) {
+                wrapPosition = Point(100 + positionX, 0)
+                wrapDirection = down
+            } else if (positionX in 50..99 && positionY == 149) {
+                wrapPosition = Point(49, 150 + (positionX - 50))
+                wrapDirection = left
+            } else if (positionX in 100..149 && positionY == 49) {
+                wrapPosition = Point(99, 50 + (positionX - 100))
+                wrapDirection = left
+            }
+        }
+
+        if (direction == left) {
+            if (positionX == 50 && positionY in 0..49) {
+                wrapPosition = Point(0, 100 + (49 - positionY))
+                wrapDirection = right
+            } else if (positionX == 50 && positionY in 50..99) {
+                wrapPosition = Point(0 + (positionY - 50), 100)
+                wrapDirection = down
+            } else if (positionX == 0 && positionY in 100..149) {
+                wrapPosition = Point(50, 0 + (149 - positionY))
+                wrapDirection = right
+            } else if (positionX == 0 && positionY in 150..199) {
+                wrapPosition = Point(50 + (positionY - 150), 0)
+                wrapDirection = down
+            }
+        }
+
+        if (direction == right) {
+            if (positionX == 149 && positionY in 0..49) {
+                wrapPosition = Point(99, 100 + (49 - positionY))
+                wrapDirection = left
+            } else if (positionX == 99 && positionY in 50..99) {
+                wrapPosition = Point(100 + (positionY - 50), 49)
+                wrapDirection = up
+            } else if (positionX == 99 && positionY in 100..149) {
+                wrapPosition = Point(149, 0 + (149 - positionY))
+                wrapDirection = left
+            } else if (positionX == 49 && positionY in 150..199) {
+                wrapPosition = Point(50 + (positionY - 150), 149)
+                wrapDirection = up
+            }
+        }
+
+        return wrapPosition!! to wrapDirection!!
     }
-    
+
     val right = Point(1, 0)
     val left = Point(-1, 0)
     val down = Point(0, 1)
     val up = Point(0, -1)
-    
+
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Point) = Point(x + other.x, y + other.y)
     }
@@ -138,8 +167,8 @@ class Day22(input: String) {
 
 fun main() {
 
-//    val input = readText("day22.txt")
-    val input = readText("day22.txt", true)
+    val input = readText("day22.txt")
+//    val input = readText("day22.txt", true)
 
     val result = Day22(input).part2()
     println(result)
