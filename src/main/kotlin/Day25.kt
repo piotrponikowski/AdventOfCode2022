7bdac7185eee742b1332e4604e1da15a6265e32d
Piotr Ponikowski <piotr-ponikowski@source-corp.com>
2022-12-25 06:44:40 +0100

    day 25 cleanup

--- src/main/kotlin/Day25.kt
+++ src/main/kotlin/Day25.kt
@@ -1,61 +1,36 @@
 import kotlin.math.pow
 
-class Day25(input: List<String>) {
+class Day25(val input: List<String>) {
+    
+    private fun fromSnafu(input: String) = input
+        .reversed()
+        .mapIndexed { index, digit -> 5.0.pow(index).toLong() * decode(digit) }
+        .sum()
 
-    val numbers = input.map { it.toCharArray().toTypedArray().toList() }
-
-    fun fromSnafu(snafu: List<Char>) = snafu.reversed().mapIndexed { index, digit ->
-        val value1 = 5.0.pow(index).toLong()
-        val value2 = when (digit) {
-            '2' -> 2L
-            '1' -> 1L
-            '0' -> 0L
-            '-' -> -1L
-            '=' -> -2L
-            else -> throw RuntimeException("Unknown digit")
+    private fun toSnafu(input: Long): String {
+        return when (input > 0) {
+            true -> toSnafu((input + 2) / 5) + encode(input % 5)
+            false -> ""
         }
-        value1 * value2
-    }.sum()
-
-
-    fun part1() {
-        var result = numbers.map { snafu -> fromSnafu(snafu) }.sum()
-
-        var result2 = ""
-        
-        while(result > 0) {
-           val p1 =  result % 5
-            result2 += p1.toString()
-            
-            result = (result+2) /5
-        }
-        
-        
-        result2 = result2.reversed()
-        result2 = result2.map { a -> 
-            when(a) {
-                '4' -> '='
-                '3' -> '-'
-                '2' -> '2'
-                '1' -> '1'
-                '0' -> '0'
-                else -> throw RuntimeException("Unknown digit")
-            }
-        }.joinToString("")
-        
-        println(result2)
     }
+
+    private fun encode(input: Long) = when (input) {
+        4L -> '-'
+        3L -> '='
+        2L -> '2'
+        1L -> '1'
+        0L -> '0'
+        else -> throw RuntimeException("Unknown digit")
+    }
+
+    private fun decode(input: Char) = when (input) {
+        '2' -> 2L
+        '1' -> 1L
+        '0' -> 0L
+        '-' -> -1L
+        '=' -> -2L
+        else -> throw RuntimeException("Unknown digit")
+    }
+
+    fun part1() = toSnafu(input.sumOf { snafu -> fromSnafu(snafu) })
 }
-
-fun main() {
-
-    val input = readLines("day25.txt")
-//    val input = readLines("day25.txt", true)
-
-    val result = Day25(input).part1()
-    println(result)
-
-}
-
-//2011-=2=-1020-1===-1
-//2011=-2-=1020=1---=1
